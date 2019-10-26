ee4408a6a8fe54e20a0824c4a21068cc438201a5
tatarch <56882040+tatarch@users.noreply.github.com>
2019-10-26 12:19:05 +0300

    added three masivy

--- masivy.php
+++ masivy.php
@@ -14,3 +14,30 @@ foreach ($fruits as $key=> $value ){
         renderData($our);
     }
 }
+
+$transportDescription = [
+    'rocket'  => 'Can fly. Super fast',
+    'bicycle' => 'Cheap but slow',
+    'car'     => 'Average price. Need roads and other infrastructure',
+    'ship'    => 'I\'m a pirate! Yo-ho-ho!'
+];
+
+foreach ($transportDescription as $key=> $value ){
+    if($key=="ship"){
+        $our=$value;
+        renderData($our);
+    }
+}
+
+$data["Ivan"]=array('name' => 'Ivan', 'age'  => 29);
+$data["Alona"]=array( 'name' => 'Alyona', 'age'  => 21);
+$data["Kostya"]=array('name' => 'Kostya', 'age'  => 22);
+
+foreach ($data as $key=> $value ){
+    if($key=="Alona"){
+        $our=$value['age'];
+        renderData($our);
+    }
+}
+
+
