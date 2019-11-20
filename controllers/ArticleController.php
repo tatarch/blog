8350c8277ba5e68bfef9cbb02474ea376ad6300e
tatarch <fifty.nice@gmail.com>
2019-11-20 21:46:48 +0200

    rout

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -4,6 +4,11 @@ class ArticleController
 {
     function default()
     {
+        echo 'Article contr m default';
+    }
 
+    function foo()
+    {
+        echo 'Article contr m foo';
     }
 }
--- index.php
+++ index.php
@@ -8,20 +8,34 @@
 //    header('Location: ' . $_SERVER['HTTP_REFERER']);
 //    die;
 //}
+
 require 'repositories/ArticleRepository.php';
 
 $url = $_SERVER['REQUEST_URI'];
 $urlParts = explode('/', $url);
-if (file_exists('controllers/' . $urlParts[1] . 'Controller.php')) {
-    require 'controllers/' . $urlParts[1] . 'Controller.php';
-    $className = $urlParts[1] . 'Controller';
-    $controller = new $className;
+
+if (!empty($urlParts[1])) {
+    if (file_exists('controllers/' . $urlParts[1] . 'Controller.php')) {
+        require 'controllers/' . $urlParts[1] . 'Controller.php';
+        $className = $urlParts[1] . 'Controller';
+        $controller = new $className;
+    } else {
+        echo 'Not Found';
+        die();
+    }
+
 } else {
-    echo 'Not Found';
-    die();
+    require 'controllers/HomeController.php';
+    $controller = new HomeController();
 }
 
-$method = $urlParts[2];
-if (method_exists($controller, $method)) {
-    $controller->$method();
+if (isset($urlParts[2])) {
+    $method = $urlParts[2];
+    if (method_exists($controller, $method)) {
+        $controller->$method();
+    } else {
+        die('Method not exists: ' . $method);
+    }
+} else {
+    $controller->default();
 }
